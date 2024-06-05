5d2bdcb64878293e9ab99d9069fffc1ff378308a
StamatisUCR <jpapa011@ucr.edu>
2024-06-04 17:02:02 -0700

    part 2 (untested)

---
+++ user/thread.c
@@ -0,0 +1,32 @@
+#include "kernel/types.h" 
+#include "kernel/stat.h" 
+#include "user/thread.h"
+#include "user/user.h"
+#define PGSIZE 4096
+
+int thread_create(void *(start_routine)(void*), void *arg) {
+    void* stack = (void*) malloc(PGSIZE);
+    int pid = clone(stack);
+    if (pid != -1) {
+        // call start routine
+        (*start_routine)(arg);
+        exit(0);
+    } else {
+        return -1;
+    }
+    return 0;
+}
+
+void lock_init(struct lock_t* lock) {
+    lock->locked = 0;
+}
+
+void lock_acquire(struct lock_t* lock) {
+    while(__sync_lock_test_and_set(&lock->locked, 1) != 0);
+    __sync_synchronize();
+}
+
+void lock_release(struct lock_t* lock) {
+    __sync_synchronize();
+    __sync_lock_release(&lock->locked, 0);
+}
